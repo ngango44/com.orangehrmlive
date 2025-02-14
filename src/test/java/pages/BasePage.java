a8ef1d394da8f1c9e96de191a5baa1261342a071
ngale44 <lenga4579@gmail.com>
2025-02-14 17:41:02 +0700

    update

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    private WebDriver driver;
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
---
+++ src/test/java/pages/LoginPages.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPages extends BasePage{
+    public LoginPages(WebDriver driver) {
+        super(driver);
+    }
+
+}
